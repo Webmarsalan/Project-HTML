84b311cbea01d1809b58419568ba4c24ae7626cc
Webmarsalan <thinkpadgadgets@gmail.com>
2025-09-27 10:42:11 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,109 @@
+// Declare three variables using var, let, and const.Print them using console.log().
+
+let age = "20";
+age = "19";
+console.log(age);
+
+let person = "Anand";
+console.log(person);
+
+let name = "Anand Rathi";
+console.log(name);
+
+var x = "20";
+console.log(x);
+
+var number = "60";
+console.log(number);
+
+var x = "45";
+console.log(x)
+
+var height = "30 foot 50 inch"
+console.log(height)
+
+const color = "Blue"
+console.log(color)
+
+const firstName = "Anand"
+console.log(firstName)
+
+const secondName = "Kumar"
+console.log(secondName)
+console.log(firstName + " " + secondName);
+
+CarName = "COROLLA";
+let CarName;
+console.log(CarName);
+
+
+
+// Create a variable called city and assign "Karachi". Reassign it to "Lahore". Try with let and const. What happens?
+
+
+// var may reassign and also redeclared
+
+var country = "itlay";
+console.log(country);
+
+var country = "USA";
+console.log(country);
+
+var country = "CANADA";
+country = "London";
+console.log(country);
+
+// Let may re assign but not redeclared
+let city = "lahore";
+console.log(city);
+
+const province = "kPK";
+console.log(province)
+
+let seat = "free"
+seat = "reserve"
+
+// const may not redeclared or nor reassign
+const town = "Dehli Colony";
+// town = "Punjab colony";
+console.log(town);
+
+// Create a variable sentence that says: I am learning JavaScript! using double quotes.
+var sentence = "I am learinig Java script!";
+console.log(sentence);
+
+// Write code that stores a number, then changes it into a string. Use typeof to check type before and after.
+
+let num = 786;
+console.log(num);
+console.log(typeof num);
+let convert = String(num);
+console.log(convert);
+console.log(typeof convert);
+
+// Combine your first name and last name into a fullName variable using both + operator and template literals.
+let first_name = "Jaideep";
+let second_name = "Kumar"
+let full_name = firstName + " " + secondName;
+console.log(full_name);
+
+// using templete
+let first = "Bhavesh";
+let second = "Kumar";
+let fullName = `${first} ${second}`;
+console.log(fullName);
+
+// Print a sentence3: "Hello, my name is [fullName] and I love coding!" using template literals.
+let sentence3 = `"Hello, my name is ${full_name} and I love coding"`;
+console.log(sentence3);
+
+// What will be the output?
+
+let course;
+console.log(course);
+
+let b = null;
+console.log(b);
+
+let vegetables = null;
+console.log(vegetables);
